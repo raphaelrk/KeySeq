555428e421f023d72b93161df9fc2665094ea336
Raphael Rouvinov-Kats <coolcade283@gmail.com>
2014-05-08 00:33:26 -0500

    Static import, common seqs array
    Added a static import for KeyEvent to reduce code clutter.
    Added some final int[]s for common key sequences.
    
    At first, before my static import, I had an array of common keys like
    int UP = KeyEvent.VK_UP, A = KeyEvent.VK_A, etc. but I scratched that
    out because if you're going to have to use VK_ for some you might as
    well leave it consistent and I didn't want to copy every single static
    member from KeyEvent. Maybe there's a way with reflection to have all
    the KeyEvents usable without VK_ in front. It would make code look a lot
    nicer if the VK_'s disappeared.

--- KeySeq/src/com/raphaelrk/keyalgorithm/KeyAlgorithm.java
+++ KeySeq/src/com/raphaelrk/keyalgorithm/KeyAlgorithm.java
@@ -1,7 +1,12 @@
 package com.raphaelrk.keyalgorithm;
 
+
 import java.awt.Robot;
-import java.awt.event.KeyEvent;
+
+/**
+ * Take all of KeyEvent's static members and import them, eliminating the need for "KeyEvent."
+ */
+import static java.awt.event.KeyEvent.*;
 
 /**
  * This program continuously presses a given keySequence,
@@ -17,10 +22,19 @@ import java.awt.event.KeyEvent;
  */
 public class KeyAlgorithm {
 
-    private int[] keySequence; // keySequence that robot will perform. indices are keys. Keys come from KeyEvent's final ints
+    private int[] keySequence; // keySequence that robot will perform. indices are keys. Keys come from KeyEvent's static final ints
     private Robot r; // Robot that will perform your keySequence
     
     private static final int DELAY = 1; // delay between each key event in milliseconds
+    
+    /**
+     * A few common key sequences one might use
+     */
+    private static final int[] LDDRDD = {VK_LEFT, VK_DOWN, VK_DOWN, VK_RIGHT, VK_DOWN, VK_DOWN},
+                               ABCS = {VK_A, VK_B, VK_C, VK_D, VK_E, VK_F, VK_G, VK_H, VK_I, VK_J, VK_K, VK_L, VK_M, 
+                                       VK_N, VK_O, VK_P, VK_Q, VK_R, VK_S, VK_T, VK_U, VK_V, VK_W, VK_X, VK_Y, VK_Z},
+                               CW = {VK_UP, VK_RIGHT, VK_DOWN, VK_LEFT},
+                               CCW = {VK_UP, VK_LEFT, VK_DOWN, VK_RIGHT};
     
     public void setAlgorithm(int[] keys) {
         keySequence = keys;
@@ -65,8 +79,11 @@ public class KeyAlgorithm {
     public static void main(String[] args) {
         KeyAlgorithm keyAlg = new KeyAlgorithm();
         
-        int[] alg = {KeyEvent.VK_LEFT, KeyEvent.VK_DOWN, KeyEvent.VK_DOWN, 
-                     KeyEvent.VK_RIGHT, KeyEvent.VK_DOWN, KeyEvent.VK_DOWN};
+        // example custom sequence:
+//        int[] alg = {VK_LEFT, VK_DOWN, VK_DOWN, 
+//                     VK_RIGHT, VK_DOWN, VK_DOWN};
+        
+        int[] alg = {VK_DOWN, VK_RIGHT};
         keyAlg.setAlgorithm(alg);
         
         keyAlg.performAlgorithm();
